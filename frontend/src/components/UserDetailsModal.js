3008e08e4bb2b0335dfe8e6bc3a178ad25cbd5d0
Rico Rein Talvar <ritalv@ttu.ee>
2019-11-16 12:06:31 +0200

    login table

--- frontend/src/components/UserDetailsModal.js
+++ frontend/src/components/UserDetailsModal.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import { ModalBody, ModalHeader, Row, Col } from 'reactstrap';
+import { ModalBody, ModalHeader, Row, Col, Table } from 'reactstrap';
 
 class UserDetailsModal extends Component {
     
     render() {
         const { toggle } = this.props;
         const { data } = this.props;
-        const mdStyle = {size: 8, order: 1, offset: 2}
+        const mdStyle = {size: 8, offset: 2}
         return (
             <div>
                 <ModalHeader toggle={() => toggle()}>{`Viewing ${data.firstName} ${data.lastName}`}</ModalHeader>
@@ -16,7 +16,16 @@ class UserDetailsModal extends Component {
                             <span className="bold">{`Contact email: ${data.email}`}</span>
                         </Col>
                         {data.login_count.length > 0 ?
-                        <Col xs={{size: 12}} md={{size: 10, offset: 1}}></Col> :
+                        <Col className="center" xs={{size: 12}} md={{size: 10, offset: 1}}>
+                            <span className="bold margin-top">Login Log</span>
+                            <Table>
+                                <tbody>
+                                    {data.login_count.map((date, i) => 
+                                        (<tr key={i}><td className="center">{new Date(date).toLocaleString()}</td></tr>)
+                                    )}
+                                </tbody>
+                            </Table>
+                        </Col> :
                         <Col className="center" md={mdStyle}>
                             <span>{`This user has not logged in yet`}</span>
                         </Col>
