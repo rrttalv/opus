bab49eb7edcaefeab210cad5afccc1cdfc8132b9
Rico Rein Talvar <ritalv@ttu.ee>
2019-11-16 11:33:06 +0200

    login count styling

--- frontend/src/components/UserDetailsModal.js
+++ frontend/src/components/UserDetailsModal.js
@@ -6,14 +6,21 @@ class UserDetailsModal extends Component {
     render() {
         const { toggle } = this.props;
         const { data } = this.props;
+        const mdStyle = {size: 8, order: 1, offset: 2}
         return (
             <div>
                 <ModalHeader toggle={() => toggle()}>{`Viewing ${data.firstName} ${data.lastName}`}</ModalHeader>
                 <ModalBody>
                     <Row>
-                        <Col className="center" md={{size: 8, order: 1, offset: 2}}>
+                        <Col className="center" md={mdStyle}>
                             <span className="bold">{`Contact email: ${data.email}`}</span>
-                            </Col>
+                        </Col>
+                        {data.login_count.length > 0 ?
+                        <Col xs={{size: 12}} md={{size: 10, offset: 1}}></Col> :
+                        <Col className="center" md={mdStyle}>
+                            <span>{`This user has not logged in yet`}</span>
+                        </Col>
+                        }
                     </Row>
                 </ModalBody>
             </div>
