577711d476eed4af4d68e0ba4b11bb27bc3a5ef0
Rico Rein Talvar <ritalv@ttu.ee>
2019-11-17 10:40:32 +0200

    verify and forgot component

---
+++ frontend/src/components/unauth/VerifyEmail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class VerifyEmail extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
